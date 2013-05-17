7f3f834f27e41d073b765e4a6b8bda4ff3d18c3f
Gelus <benjarmil@gmail.com>
2013-05-17 12:01:34 -0600

    README update. index

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 <h2>Javascript Pollyfils</h2>
 <p>A growing collection.</p>
+<hr>
+<h4>index</h4>
+<p>array.prototype.map<br />
+	<ul>
+		<small>tested in:</small>
+		<li>Chrome</li>
+		<li>Firefox</li>
+		<li>IE9, 8, 7</li>
+	</ul></p>
--- array.map.js
+++ array.map.js
@@ -4,7 +4,6 @@
 			if(typeof f != "function")throw new TypeError(200, typeof f+" is not a function");
 			var Rar = []
 				,	i = 0;
-				console.log(this.length);
 			while(i<this.length){
 				if(i in this)Rar.push(f.call(t, this[i]));
 				i++
