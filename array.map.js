01348ac422ec9b2c9267d31633c91493627931a7
Gelus <benjarmil@gmail.com>
2013-05-17 11:51:06 -0600

    init commit - array.map fill

---
+++ array.map.js
@@ -0,0 +1,14 @@
+(function(){
+	if([].map)return
+		Array.prototype.map = function(f,t){
+			if(typeof f != "function")throw new TypeError(200, typeof f+" is not a function");
+			var Rar = []
+				,	i = 0;
+				console.log(this.length);
+			while(i<this.length){
+				if(i in this)Rar.push(f.call(t, this[i]));
+				i++
+			}
+			return Rar;
+		}
+})()
