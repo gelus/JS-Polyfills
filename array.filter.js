2e14d2b003c9cc3da055701f00050385ffee427e
Gelus <benjarmil@gmail.com>
2013-05-20 12:26:14 -0600

    array.filter fill

---
+++ array.filter.js
@@ -0,0 +1,15 @@
+(function(){
+	if([].filter)return
+		Array.prototype.filter = function(f,t){
+			if(typeof f != "function")throw new TypeError(200, typeof f+" is not a function");	
+			var Rar = []
+				,	i = 0;
+			while(i<this.length){
+				if(i in this){
+					if(f.call(t,this[i]))Rar.push(this[i]);
+				}
+				i++
+			}
+			return Rar;
+		}
+})()
