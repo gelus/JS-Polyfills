06f6f53ebdc871dd92d39e5ee09950a38c20dbb0
Gelus <benjarmil@gmail.com>
2013-05-24 10:48:37 -0600

    array.indexOf filled array.lastIndexOf filled

---
+++ array.indexOf.js
@@ -0,0 +1,11 @@
+(function(){
+	if([].indexOf)return
+	Array.prototype.indexOf = function(v,i){
+		var i = (i)? i:0;
+		while(i<this.length){
+			if(i in this && this[i] == v)return i
+			i++
+		}
+		return -1;
+	}
+})()
---
+++ array.lastIndexOf.js
@@ -0,0 +1,11 @@
+(function(){
+	if([].lastIndexOf)return
+	Array.prototype.lastIndexOf = function(v,i){
+		var i = (i)? i:this.length;
+		while(i>=0){
+			if(i in this && this[i] == v)return i
+			i--
+		}
+		return -1;
+	}
+})()
